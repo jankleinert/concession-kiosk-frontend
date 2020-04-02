3562d06cb559cee1444c1f2f15fa821f8537dc5e
ilyesbourigua <ilyesbouriga1@gmail.com>
2020-04-02 11:36:34 +0100

    changes test2

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/thankyou', thankyouRouter);
+app.use('/allusers', allusersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
