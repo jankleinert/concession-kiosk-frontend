64e061230ac8d35fb6dcdb484a8b074d45aabc96
ilyesbourigua <ilyesbouriga1@gmail.com>
2020-04-02 11:51:23 +0100

    changes test4

--- routes/allusers.js
+++ routes/allusers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET Users list */
-router.post('/', function (req, res, next) {
+router.get('/allusers', function (req, res, next) {
     let url = "http://" + host + ":" + port + "/allusers";
   
     request.get(url, function( err, response, body) {
@@ -10,6 +10,7 @@ router.post('/', function (req, res, next) {
         res.render('thankyou', { userNumber: -1 });
       } else {
         let userInfo = JSON.parse(body);
+        console.log(userInfo);
         res.render('thankyou', { users: userInfo  });
         
       }
