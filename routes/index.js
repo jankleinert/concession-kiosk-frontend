f02ec08d570ac40845f100c483cb5d2e4d8472be
ilyesbourigua <ilyesbouriga1@gmail.com>
2020-04-02 09:36:11 +0100

    update front

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,16 @@ router.get('/', function(req, res, next) {
 
 /* POST main page */
 router.post('/', function (req, res, next) {
-  let url = "http://" + host + ":" + port + "/ticketNumber?hotdog=" +
-             req.body.hotdog + "&hamburger=" + req.body.hamburger + "&salad=" +
-             req.body.salad + "&pizza=" + req.body.pizza + "&soda=" +req.body.soda; 
+  let url = "http://" + host + ":" + port + "/ticketNumber?firstname=" +
+             req.body.firstname + "&lastname=" + req.body.lastname + "&age=" +
+             req.body.age + "&email=" + req.body.email + "&occupation=" +req.body.occupation; 
 
   request.get(url, function( err, response, body) {
     if(err){
       res.render('thankyou', { ticketNumber: -1 });
     } else {
       let ticketInfo = JSON.parse(body);
-      res.render('thankyou', { ticketNumber: ticketInfo.result, order: ticketInfo.order  });
+      res.render('thankyou', { ticketNumber: ticketInfo.result, user: ticketInfo.user  });
       
     }
   });
