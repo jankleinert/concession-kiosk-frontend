c95f0a1e69f62f3b9197e06b0b6a6706361dfb12
ilyesbourigua <ilyesbouriga1@gmail.com>
2020-04-03 11:03:39 +0100

    changes test5

--- routes/allusers.js
+++ routes/allusers.js
@@ -1,25 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
-var host = process.env.COMPONENT_BACKEND_HOST || '0.0.0.0';
-var port = process.env.COMPONENT_BACKEND_PORT || 8080;
-/* GET Users list */
+const express = require('express');
+const router = express.Router();
 
-router.get('/allusers', function(req, res, next) {
-    res.render('index');
-  });
-router.post('/allusers', function (req, res, next) {
-    let url = "http://" + host + ":" + port + "/allusers";
+/* GET request for thank you page */
+router.get('/', function(req, res, next) {
+  console.log('req: ' + req);
   
-    request.get(url, function( err, response, body) {
-      if(err){
-        res.render('allusers', { error: err });
-      } else {
-        let userInfo = JSON.parse(body);
-        console.log(userInfo);
-        res.render('allusers', { users: userInfo  });
-        
-      }
-    });
-})
+  res.render('allusers');
+});
+
+router.get('')
+
 module.exports = router;
